8092da9eebf549e004dabd472866ffe5897dad42
Yixuan Wang <wang.yixuan@live.fr>
2023-03-09 19:37:05 -0500

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Hello, World!</h1>
+</template>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Hanken Grotesk', sans-serif;
+}
+
+body {
+    background-color: #271c36;
+    color: #fff;
+}
+</style>
